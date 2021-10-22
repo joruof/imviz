a447b039fe0cf374cdeda9dc512e739ff3ae186b
Jona Ruof <jona.ruof@uni-ulm.de>
2021-10-22 12:46:27 +0200

    version bump

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 
 setup(name="imviz",
-      version="0.1.12",
+      version="0.1.13",
       description="Pythonic bindings for imgui/implot",
       url="https://github.com/joruof/imviz",
       author="Jona Ruof",
