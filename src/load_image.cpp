128849958e33a8a9884b5f8c49d01d5bb0bb956b
Jona Ruof <jona.ruof@uni-ulm.de>
2022-04-19 08:20:46 +0200

    added forgotten files

---
+++ src/load_image.cpp
@@ -0,0 +1,23 @@
+#include "load_image.hpp"
+
+#include <stdexcept>
+
+/**
+ * This allows us to handle stb_image assertions via exceptions on the python side.
+ */
+void checkStbImageAssertion(bool expr, const char* exprStr) {
+
+    if (not expr) { 
+        throw std::runtime_error(exprStr);
+    }
+}
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ASSERT(_EXPR) checkStbImageAssertion(_EXPR, #_EXPR)
+
+#include "stb_image.h"
+
+unsigned char* loadImage(const char* filename, int* x, int* y, int* n, int wantedChannels) {
+    
+    return stbi_load(filename, x, y, n, wantedChannels);
+}
---
+++ src/load_image.hpp
@@ -0,0 +1,5 @@
+/*
+ * Super simple, single function interface for loading images.
+ * Nothings more to see. Move on!
+ */
+unsigned char* loadImage(const char* filename, int* x, int* y, int* n, int wantedChannels);
