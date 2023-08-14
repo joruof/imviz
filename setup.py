6f67caff58c04cffb30b1eba3844bef6eca69138
Jona Ruof <jona.ruof@uni-ulm.de>
2023-08-14 14:35:03 +0200

    version bump 2.1

--- setup.py
+++ setup.py
@@ -119,7 +119,7 @@ class CMakeBuild(build_ext):
 
 
 setup(name="imviz",
-      version="0.2.0",
+      version="0.2.1",
       description="Pythonic bindings for imgui/implot",
       url="https://github.com/joruof/imviz",
       author="Jona Ruof",
