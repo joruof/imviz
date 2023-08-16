93ff949f0b6afffa12e936e0ea2c284ed7ef9c50
Jona Ruof <jona.ruof@uni-ulm.de>
2023-08-16 15:51:18 +0000

    update 0.0.4, autoreload rework

--- imviz/dev.py
+++ imviz/dev.py
@@ -1,12 +1,12 @@
 import os
 import sys
-import inspect
 import datetime
 import traceback
 import platform
 from pydoc import locate
 
 import imviz as viz
+import minireload as mr
 
 
 def launch(cls, func_name):
@@ -44,154 +44,135 @@ def try_load_source(path):
         return ""
 
 
-def render_source(code_str, error_line, new_stack_frame_sel):
+def build_launcher(cls, func_name):
 
-    if viz.begin_window("Source code"):
-        if viz.begin_table("code", 2,
-                           viz.TableFlags.BORDERS_INNER_V):
+    class ImvizLauncher:
 
-            viz.table_setup_column(
-                    "line_numbers",
-                    viz.TableColumnFlags.WIDTH_FIXED
-                    | viz.TableColumnFlags.NO_RESIZE)
+        def __init__(self):
 
-            viz.table_setup_column(
-                    "source",
-                    viz.TableColumnFlags.WIDTH_STRETCH)
+            viz.configure_ini_path(sys.modules[cls.__module__])
 
-            for i, l in enumerate(code_str):
-                if i == error_line:
-                    lineno_color = (1.0, 0.3, 0.3)
-                    source_color = (1.0, 0.3, 0.3)
-                    if new_stack_frame_sel:
-                        viz.set_scroll_here_y(0.5)
-                        new_stack_frame_sel = False
+            self.obj = cls()
+            self.func = getattr(self.obj, func_name)
+
+            self.exc_info = None
+            self.exc_frame_idx = 0
+            self.exc_code = None
+            self.request_scroll = False
+
+        def loop(self):
+
+            self.func()
+
+        def exc_func(self, ei):
+
+            if self.exc_info != ei:
+                self.request_scroll = True
+                self.exc_info = ei
+                self.exc_frame_idx = max(0, len(ei.exc_frames) - 1)
+
+                if isinstance(ei.exc, SyntaxError):
+                    self.exc_code = try_load_source(ei.exc.filename)
                 else:
-                    lineno_color = (0.4, 0.4, 0.4)
-                    source_color = (1.0, 1.0, 1.0)
+                    self.exc_code = try_load_source(
+                            ei.exc_frames[-1].filename)
 
-                viz.table_next_column()
-                viz.text(str(i+1), color=lineno_color)
-                viz.table_next_column()
-                viz.text(l, color=source_color)
-                viz.table_next_row()
-            viz.end_table()
-    viz.end_window()
+            if not viz.wait():
+                sys.exit()
 
-    return new_stack_frame_sel
+            cx, cy = viz.get_viewport_center()
+            w, h = viz.get_main_window_size()
+            w *= 0.95
+            h *= 0.95
 
+            if viz.begin_window("Exception"):
+                fade = min(1.0, (
+                    datetime.datetime.now().timestamp()
+                    - ei.exc_time.timestamp()) / 1.0)
+                col = (1.0, fade, fade)
 
-def loop(cls, func_name):
+                time_string = ei.exc_time.strftime("%H:%M:%S")
 
-    viz.configure_ini_path(sys.modules[cls.__module__])
+                if viz.button("Reload"):
+                    return True
 
-    obj = cls()
-    func = getattr(obj, func_name)
+                viz.separator()
 
-    exc_str = None
-    exc_time = None
-    exc_type = None
-    exc_value = None
-    exc_tb = None
-    exc_frames = None
-    exc_frame_idx = -1
-    exc_code = None
+                viz.text(f"Exception time {time_string}\n", color=col)
+                viz.text(ei.exc_str, color=col)
+                viz.text("\n")
 
-    new_stack_frame_sel = False
+                viz.separator()
 
-    while True:
-        try:
-            if viz.update_autoreload():
-                exc_str = None
+                for i, f in enumerate(ei.exc_frames):
+                    if viz.selectable(
+                            f"{os.path.basename(f.filename)}"
+                            + f" at line {f.lineno} in {f.function}",
+                            i == self.exc_frame_idx):
+                        self.exc_frame_idx = i
+                        self.exc_code = try_load_source(f.filename)
+                        self.request_scroll = True
 
-            if exc_str is None:
-                func()
-            else:
-                if not viz.wait(powersave=True):
-                    sys.exit()
+            viz.end_window()
 
-                cx, cy = viz.get_viewport_center()
-                w, h = viz.get_main_window_size()
-                w *= 0.95
-                h *= 0.95
+            if viz.begin_window("App state"):
+                viz.autogui(self.obj.__dict__)
+            viz.end_window()
 
-                if viz.begin_window("Exception"):
-
-                    fade = min(1.0, (
-                        datetime.datetime.now().timestamp()
-                        - exc_time.timestamp()) / 1.0)
-                    col = (1.0, fade, fade)
-
-                    time_string = exc_time.strftime("%H:%M:%S")
-
-                    if viz.button("Reload"):
-                        exc_str = None
-                        continue
-
-                    viz.separator()
-
-                    viz.text(f"Exception time {time_string}\n", color=col)
-                    viz.text(exc_str, color=col)
-                    viz.text("\n")
-
-                    viz.separator()
-
-                    for i, f in enumerate(exc_frames):
-                        if viz.selectable(
-                                f"{os.path.basename(f.filename)}"
-                                + f" at line {f.lineno} in {f.function}",
-                                i == exc_frame_idx):
-                            exc_frame_idx = i
-                            exc_code = try_load_source(f.filename)
-                            new_stack_frame_sel = True
-
+            if isinstance(ei.exc_value, SyntaxError):
+                viz.begin_window("Local variables")
                 viz.end_window()
 
-                if viz.begin_window("App state"):
-                    viz.autogui(obj.__dict__)
+                self.render_source(
+                        self.exc_code,
+                        ei.exc_value.lineno-1)
+            else:
+                if viz.begin_window("Local variables"):
+                    f_locals = ei.exc_frames[self.exc_frame_idx].frame.f_locals
+                    try:
+                        viz.AutoguiContext(ignore_custom=True).render(f_locals)
+                    except:
+                        viz.text("Could not render local variables!", color="red")
                 viz.end_window()
 
-                if isinstance(exc_value, SyntaxError):
+                self.render_source(
+                        self.exc_code,
+                        ei.exc_frames[self.exc_frame_idx].lineno-1)
 
-                    viz.begin_window("Local variables")
-                    viz.end_window()
+            return False
 
-                    new_stack_frame_sel = render_source(
-                            exc_code,
-                            exc_value.lineno-1,
-                            new_stack_frame_sel)
+        def render_source(self, code_str, error_line):
 
-                else:
-                    if viz.begin_window("Local variables"):
-                        f_locals = exc_frames[exc_frame_idx].frame.f_locals
-                        try:
-                            viz.AutoguiContext(ignore_custom=True).render(f_locals)
-                        except:
-                            viz.text("Could not render local variables!", color="red")
-                    viz.end_window()
+            if viz.begin_window("Source code"):
+                if viz.begin_table("code", 2,
+                                   viz.TableFlags.BORDERS_INNER_V):
 
-                    new_stack_frame_sel = render_source(
-                            exc_code,
-                            exc_frames[exc_frame_idx].lineno-1,
-                            new_stack_frame_sel)
+                    viz.table_setup_column(
+                            "line_numbers",
+                            viz.TableColumnFlags.WIDTH_FIXED
+                            | viz.TableColumnFlags.NO_RESIZE)
 
-        except SystemExit:
-            return
-        except Exception as e:
-            traceback.print_exc()
+                    viz.table_setup_column(
+                            "source",
+                            viz.TableColumnFlags.WIDTH_STRETCH)
 
-            # collect information about the exception
-            exc_str = traceback.format_exc()
-            exc_time = datetime.datetime.now()
-            (exc_type, exc_value, exc_tb) = sys.exc_info()
+                    for i, l in enumerate(code_str):
+                        if i == error_line:
+                            lineno_color = (1.0, 0.3, 0.3)
+                            source_color = (1.0, 0.3, 0.3)
+                            if self.request_scroll:
+                                viz.set_scroll_here_y(0.5)
+                                self.request_scroll = False
+                        else:
+                            lineno_color = (0.4, 0.4, 0.4)
+                            source_color = (1.0, 1.0, 1.0)
 
-            if isinstance(e, SyntaxError):
-                exc_frames = []
-                exc_frame_idx = 0
-                exc_code = try_load_source(e.filename)
-            else:
-                exc_frames = inspect.getinnerframes(exc_tb)
-                exc_frame_idx = max(0, len(exc_frames) - 1)
-                exc_code = try_load_source(exc_frames[-1].filename)
+                        viz.table_next_column()
+                        viz.text(str(i+1), color=lineno_color)
+                        viz.table_next_column()
+                        viz.text(l, color=source_color)
+                        viz.table_next_row()
+                    viz.end_table()
+            viz.end_window()
 
-            new_stack_frame_sel = True
+    return ImvizLauncher
